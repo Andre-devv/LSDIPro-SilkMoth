e386270f4d2fab86ccda2ff7d19844290aaf9b1c
berscjak <jakobderber@gmail.com>
2025-05-20 15:53:50 +0200

    Added maximum matching verfication using networkx

--- src/silkmoth/silkmoth_engine.py
+++ src/silkmoth/silkmoth_engine.py
@@ -7,16 +7,17 @@ from silkmoth.verifier import *
 
 class SilkMothEngine:
     
-    def __init__(self, related_thresh, source_sets, sim_metric=similar, sim_func=jaccard_similarity, sim_thresh=0):
-        self.related_thresh = related_thresh        # theta
+    def __init__(self, related_thresh, source_sets, sim_metric=similar, sim_func=jaccard_similarity, sim_thresh=0, reduction=False):
+        self.related_thresh = related_thresh        # delta
         self.source_sets = source_sets              # S
         self.sim_metric = sim_metric                # related
         self.sim_func = sim_func                    # phi
         self.sim_thresh = sim_thresh                # alpha
+        self.reduction = reduction
         self.tokenizer = Tokenizer(sim_func)
         self.signature_gen = SignatureGenerator()
         self.candidate_selector = CandidateSelector()
-        self.verifier = Verifier()
+        self.verifier = Verifier(related_thresh, sim_metric, sim_func, sim_thresh, reduction)
         self.inverted_index = self.build_index(source_sets)
         
     def build_index(self, source_sets):
@@ -27,7 +28,7 @@ class SilkMothEngine:
         r_tokens = self.tokenizer.tokenize(reference_set)
         signature = self.signature_gen.get_signature(r_tokens)
         candidates = self.candidate_selector.get_candidates(signature, self.inverted_index)
-        return self.verifier.get_related_sets(candidates, r_tokens)
+        return self.verifier.get_related_sets(r_tokens, candidates, self.inverted_index)
 
     def discover_sets(self, reference_sets):
         pass
--- src/silkmoth/verifier.py
+++ src/silkmoth/verifier.py
@@ -1,8 +1,67 @@
+from silkmoth.inverted_index import InvertedIndex
+import networkx as nx
 
 class Verifier:
 
-    def __init__(self):
-        pass
+    def __init__(self, related_thresh, sim_metric, sim_func, sim_thresh=0, reduction=False):
+        """
+        Initialize the verifier with some parameters.
 
-    def get_related_sets(self, candidates):
-        pass
+        Args:
+            related_thresh: Relatedness threshold delta
+            sim_metric (callable): Similarity metric similar(...)/contain(...)
+            sim_func (callable): Similarity function phi
+            sim_thresh: Similarity threshold alpha
+            reduction (bool): Flag to activate/deactivate triangle inequality reduction
+        """
+        self.related_thresh = related_thresh
+        self.sim_metric = sim_metric
+        self.sim_func = sim_func
+        self.sim_thresh = sim_thresh
+        self.reduction = reduction
+
+    def _verify(self, reference_set, source_set) -> bool:
+        """
+        Checks if two sets are related or not by computing the maximum weighted
+        bipartite matching.
+
+        Args:
+            reference_set: Tokenized reference set R
+            source_set: Tokenized source set S
+
+        Returns:
+            bool: True if relatedness threshold is reached, False otherwise
+        """
+        r_size = len(reference_set)
+        s_size = len(source_set)
+        G = nx.Graph()
+        for r_idx, r_elem in enumerate(reference_set):
+            for s_idx, s_elem in enumerate(source_set):
+                w = self.sim_func(r_elem, s_elem, self.sim_thresh)
+                G.add_edge(r_idx, s_idx + r_size, weight=w)
+        
+        matching = nx.max_weight_matching(G)
+
+        mm_score = sum(G[u][v]['weight'] for u, v in matching)
+        relatedness = self.sim_metric(r_size, s_size, mm_score)
+        return relatedness >= self.related_thresh
+
+
+    def get_related_sets(self, reference_set: list, candidates: set, inverted_index: InvertedIndex) -> list:
+        """
+        Gives all candidate sets that are related to the reference set.
+
+        Args:
+            reference_set (list): Tokeinized reference set R
+            candidates (set): Collection of indices of candidate sets
+            inverted_index (InvertedIndex): Inverted index instance
+
+        Returns:
+            set: Indices of all related sets from the candidates
+        """
+        related_sets = set()
+        for c in candidates:
+            source_set = inverted_index.get_set(c)
+            if self._verify(reference_set, source_set):
+                related_sets.add(c)
+        return related_sets
