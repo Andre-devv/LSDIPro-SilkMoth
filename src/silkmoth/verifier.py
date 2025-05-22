8b8d29f15fd58e325ac1685aca208fdbd9382283
berscjak <jakobderber@gmail.com>
2025-05-22 18:24:04 +0200

    Added triangle inequality reduction

--- src/silkmoth/verifier.py
+++ src/silkmoth/verifier.py
@@ -1,6 +1,30 @@
 from silkmoth.inverted_index import InvertedIndex
 import networkx as nx
 
+def _reduce(reference_set: list, source_set: list) -> tuple:
+    """
+    Applies the triangle inequality reduction by removing every element from 
+    both sets that has an identical match in the other set. 
+
+    Args:
+        reference_set: Tokenized reference set R
+        source_set: Tokenized source set S
+
+    Returns:
+        (list, list, int): Reduced reference set, reduced source set and number
+        of identical elements.
+    """
+    r_reduced = reference_set[:]
+    s_reduced = source_set[:]
+    count = 0
+    for elem in reference_set:
+        if elem in s_reduced:
+            s_reduced.remove(elem)
+            r_reduced.remove(elem)
+            count += 1
+    return (r_reduced, s_reduced, count)
+
+
 class Verifier:
 
     def __init__(self, related_thresh, sim_metric, sim_func, sim_thresh=0, reduction=False):
@@ -20,6 +44,31 @@ class Verifier:
         self.sim_thresh = sim_thresh
         self.reduction = reduction
 
+    def _get_mm_score(self, reference_set, source_set) -> float:
+        """
+        Computes the maximum weighted bipartite matching score, where elements 
+        correspond to nodes and the edges are weighted using the similarity 
+        function.
+
+        Args:
+            reference_set: Tokenized reference set R
+            source_set: Tokenized source set S
+        
+        Returns:
+            float: Maximum matching score (sum of weights of edges in the 
+            matching)
+        """
+        G = nx.Graph()
+        for r_idx, r_elem in enumerate(reference_set):
+            for s_idx, s_elem in enumerate(source_set):
+                w = self.sim_func(r_elem, s_elem, self.sim_thresh)
+                G.add_edge(r_idx, s_idx + len(reference_set), weight=w)
+        
+        matching = nx.max_weight_matching(G)
+        return sum(G[u][v]['weight'] for u, v in matching)
+    
+
+
     def _verify(self, reference_set, source_set) -> bool:
         """
         Checks if two sets are related or not by computing the maximum weighted
@@ -34,15 +83,11 @@ class Verifier:
         """
         r_size = len(reference_set)
         s_size = len(source_set)
-        G = nx.Graph()
-        for r_idx, r_elem in enumerate(reference_set):
-            for s_idx, s_elem in enumerate(source_set):
-                w = self.sim_func(r_elem, s_elem, self.sim_thresh)
-                G.add_edge(r_idx, s_idx + r_size, weight=w)
-        
-        matching = nx.max_weight_matching(G)
+        exact_matches = 0
+        if self.reduction:
+            reference_set, source_set, exact_matches = _reduce(reference_set, source_set)
 
-        mm_score = sum(G[u][v]['weight'] for u, v in matching)
+        mm_score = self._get_mm_score(reference_set, source_set) + exact_matches
         relatedness = self.sim_metric(r_size, s_size, mm_score)
         return relatedness >= self.related_thresh
 
