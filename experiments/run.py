3c59e4cdc33fc635d4929cf29c8026092a6c9af3
ChibbsChubby <iqbal05064@gmail.com>
2025-06-05 04:21:21 +0200

    added small changes to dblp loader and experiment

--- experiments/run.py
+++ experiments/run.py
@@ -3,7 +3,5 @@ from experiments import Experiments
 if __name__ == "__main__":
     # Run all experiments
     exp = Experiments()
-    #exp.run_webtable_approximate_inclusion_dependency_experiment()
-
-    # run with SILKMOTH_DATA_PATH=./experiments/data PYTHONPATH=src python3 experiments/run.py
+    exp.run_webtable_approximate_inclusion_dependency_experiment()
     exp.run_dblp_approximate_string_matching_experiment()
